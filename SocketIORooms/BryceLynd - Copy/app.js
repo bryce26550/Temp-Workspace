c493f347ab24e8186f22c2ab9c78b33d6a74c223
bryce26550 <129105196+bryce26550@users.noreply.github.com>
2024-12-11 11:33:50 -0500

    update 12/11/24

---
+++ SocketIORooms/BryceLynd - Copy/app.js
@@ -0,0 +1,118 @@
+const ejs = require("ejs");
+const sqlite3 = require("sqlite3").verbose();
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const { error, profile } = require("console");
+const port = 3000;
+const AUTH_URL = 'http://172.16.3.100:420/oauth';
+const THIS_URL = 'http://localhost:3000/login';
+
+const db = new sqlite3.Database(`theDatabase.db`, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'H1!l!k3$3@0fTH3!^3$',
+    resave: false,
+    saveUninitialized: false
+}));
+
+function isAuthenticated(req, res, next) {
+    if (req.session.user) next();
+    else res.redirect('/login');
+}
+
+app.get('/', (req, res) => {
+    try {
+        res.redirect('/login');
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.get('/login', (req, res) => {
+    if (req.query.token) {
+        let tokenData = jwt.decode(req.query.token);
+        req.session.token = tokenData;
+        req.session.user = tokenData.username;
+
+        let fb_id = req.session.token.id;
+        let fb_name = req.session.user;
+        let query = `SELECT * FROM users WHERE fb_id = ?`;
+
+        db.get(query, [fb_id], (err, row) => {
+            if (err) {
+                console.log(err);
+                console.error(err);
+                res.send("There was an error:\n" + err)
+            } else if (row) {
+                res.render("chat", { user: fb_name });
+            } else {
+                db.run(`INSERT INTO users(fb_name, fb_id, profile_checked) VALUES(?, ?, ?)`, [fb_name, fb_id, 0], (err) => {
+                    if (err) {
+                        console.log(err);
+                        console.error(err);
+                        res.send("There was an error:\n" + err)
+                    } else {
+                        res.render("chat", { user: fb_name });
+                    }
+                });
+            }
+        });
+    } else {
+        res.redirect(`${AUTH_URL}?redirectURL=${THIS_URL}`);
+    }
+});
+
+app.get("/chat", isAuthenticated, (req, res) => {
+    let fb_name = req.session.user;
+    if (req.session.token && req.session.token.id) {
+        let fb_id = req.session.token.id;
+
+        db.get("SELECT * FROM users WHERE fb_id = ?", [fb_id], (err, user) => {
+            if (err) {
+                console.error(err);
+                res.send("There was an error:\n" + err)
+            } else {
+                res.render("chat", { user: fb_name });
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on('user_connected', (username) => {
+        console.log(`${username} has connected`);
+    });
+
+    socket.on('join_room', (data) => {
+        let { user, room } = data;
+        socket.join(room);
+        console.log(`${user} joined room: ${room}`);
+    });
+
+    socket.on('message', (data) => {
+        console.log('Message received on server:', data);
+        io.to(data.room).emit('message', data);
+    });
+
+    socket.on('user_disconnected', (username) => {
+        console.log(`${username} has disconnected`);
+    });
+});
